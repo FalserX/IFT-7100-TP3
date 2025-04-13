c61c0bdd22d4fefaf09b8b5ee3d9705655192a78
William Malenfant <22843250+FalserX@users.noreply.github.com>
2025-04-13 15:55:59 -0400

    complete current languages settings

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("Home");
+  const t = useTranslations();
   return (
     <main className="min-h-[85vh] bg-white rounded-2xl">
-      <h2 className="items-center text-black">{t("test")}</h2>
+      <h2 className="items-center text-black">{t("home.test")}</h2>
     </main>
   );
 }
--- src/components/page-logo/page-logo.tsx
+++ src/components/page-logo/page-logo.tsx
@@ -1,30 +1,46 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 
 type PageLogoProps = {
   siteName: string;
   href: string;
+  imgSrc: string;
+  imgAlt: string;
+  tooltip?: string;
 };
 
-const PageLogo = ({ siteName, href }: PageLogoProps) => {
-  const params = useParams();
-  const currentLocale = params?.locale ? (params.locale as string) : "fr";
+const PageLogo = ({
+  siteName,
+  href,
+  tooltip,
+  imgAlt,
+  imgSrc,
+}: PageLogoProps) => {
   return (
     <Link
       href={href}
-      locale={currentLocale}
-      className="inline-flex justify-center items-center"
+      className={`inline-flex ${
+        tooltip ? "relative group" : ""
+      } justify-center items-center`}
     >
       <Image
         className="size-12 shadow-2xl rounded-2xl items-center justify-center"
-        alt="LogoMagasin"
-        src="/Logo.svg"
+        alt={imgAlt}
+        src={imgSrc}
         width={32}
         height={32}
       />
-      <p className="ml-5">{siteName}</p>
+      {tooltip ? (
+        <span
+          className={`absolute top-full left-1/2 transform -translate-x-1/2 translate-y-4 mb-2 px-2 py-1 text-xs text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300`}
+        >
+          {tooltip}
+        </span>
+      ) : (
+        <></>
+      )}
+      <span className="ml-5">{siteName}</span>
     </Link>
   );
 };
